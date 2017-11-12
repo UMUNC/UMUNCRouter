a55c1d2a320446d3bbd51c2e8dc759de7da54d44
eastpiger <lvwt@shanghaitech.edu.cn>
2017-11-13 00:49:23 +0800

    fix error on environment varibles

--- loader.sh
+++ loader.sh
@@ -2,27 +2,27 @@
 # for var in $testt ; do
 # done
 
-if [ "$nc" = "nc" ]
+if [ $nc = "nc" ]
 then
 	cp /etc/nginx/sites-available/nc.conf /etc/nginx/sites-enabled/;
 fi
 
-if [ "$nw" = "nw" ]
+if [ $nw = "nw" ]
 then
 	cp /etc/nginx/sites-available/nw.conf /etc/nginx/sites-enabled/;
 fi
 
-if [ "$sw" = "sw" ]
+if [ $sw = "sw" ]
 then
 	cp /etc/nginx/sites-available/sw.conf /etc/nginx/sites-enabled/;
 fi
 
-if [ "$na" = "na" ]
+if [ $na = "na" ]
 then
 	cp /etc/nginx/sites-available/na.conf /etc/nginx/sites-enabled/;
 fi
 
-if [ "$cc" = "cc" ]
+if [ $cc = "cc" ]
 then
 	cp /etc/nginx/sites-available/cc.conf /etc/nginx/sites-enabled/;
 fi
