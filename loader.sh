cad5c1443a1b74822c98a1f055092cca4409bed1
eastpiger <lvwt@shanghaitech.edu.cn>
2017-11-12 23:38:25 +0800

    add missing files

---
+++ loader.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# for var in $testt ; do
+# done
+
+if [ "$nc" = "nc" ]
+then
+	cp /etc/nginx/sites-available/nc.conf /etc/nginx/sites-enabled/;
+fi
+
+if [ "$nw" = "nw" ]
+then
+	cp /etc/nginx/sites-available/nw.conf /etc/nginx/sites-enabled/;
+fi
+
+if [ "$sw" = "sw" ]
+then
+	cp /etc/nginx/sites-available/sw.conf /etc/nginx/sites-enabled/;
+fi
+
+if [ "$na" = "na" ]
+then
+	cp /etc/nginx/sites-available/na.conf /etc/nginx/sites-enabled/;
+fi
+
+if [ "$cc" = "cc" ]
+then
+	cp /etc/nginx/sites-available/cc.conf /etc/nginx/sites-enabled/;
+fi
+
+
+nginx -g "daemon off;"
